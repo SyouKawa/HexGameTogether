d7f259ae7b8588f055901f2d28016630c695580e
bisyoufish <chernoar_syoukawa_bisyoufish@outlook.com>
2019-12-14 23:41:38 +0800

    寻路及人物测试

---
+++ Assets/Scripts/Maps/PathManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    private static PathManager _instance;
+    public static PathManager Instance { get { return _instance; } }
+
+    private void Awake(){
+        _instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+        }
+    }
+}
