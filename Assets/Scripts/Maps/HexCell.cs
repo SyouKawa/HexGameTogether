438ade5a32b21e88cb28451078770493125de7ac
bisyoufish <chernoar_syoukawa_bisyoufish@outlook.com>
2019-12-19 22:17:54 +0800

    染色寻路Temp(未优化)

--- Assets/Scripts/Maps/HexCell.cs
+++ Assets/Scripts/Maps/HexCell.cs
@@ -11,13 +11,18 @@ public class HexCell
     public SpriteRenderer Img;
     public UnityEngine.UI.Text text;
     public GameStaticData.FieldType type;
-    public int passCost;
+
+    public int fieldcost;//通过该节点本身的消耗
+    public float dynamicost;//G值:从起点到该节点的消耗(pre+filed之和)
+    public float destdis;//H值:估计值,用该点到终点的曼哈顿距离充当
+    public HexCell prepathcell;
 
     public HexCell(Vector2Int _MapPos) {
         MapPos = _MapPos;
         cell = ObjectManager.GetInstantiate(this);
         Img = cell.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
         SetFieldType(GameStaticData.FieldType.EdgeSea);//默认不可行动
+        ResetFindPathData();
     }
 
     ~HexCell() {
@@ -36,9 +41,9 @@ public class HexCell
     public void SetFieldType(GameStaticData.FieldType _type) {
         type = _type;
         switch (type) {
-            case GameStaticData.FieldType.Forest : passCost = 3; break;
-            case GameStaticData.FieldType.Mountain: passCost = 10; break;
-            case GameStaticData.FieldType.Plain: passCost = 1; break;
+            case GameStaticData.FieldType.Forest : fieldcost = 5; break;
+            case GameStaticData.FieldType.Mountain: fieldcost = 10; break;
+            case GameStaticData.FieldType.Plain: fieldcost = 1; break;
         }
     }
 
@@ -48,5 +53,14 @@ public class HexCell
 
     public void SetText(string str) {
         text.text = text.text + "\n" + str;
+    }
+    /// <summary>
+    /// 重置寻路时所需要的数据
+    /// </summary>
+    public void ResetFindPathData() {
+        fieldcost = 0;
+        dynamicost = 0;
+        destdis = 0;
+        prepathcell = null;
     }
 }
--- Assets/Scripts/Maps/PathManager.cs
+++ Assets/Scripts/Maps/PathManager.cs
@@ -11,15 +11,15 @@ public class PathManager : MonoBehaviour
     //存放目的地列表
     public List<HexCell> DestPoints;
     public HexCell Begin;
-    public Dictionary<int,HexCell> loadingPath;
-    public Dictionary<int,HexCell> closed;
+    public List<HexCell> finalPath;
+    public HashSet<HexCell> closed;
     public Dictionary<HexCell, float> open;
 
     private void Awake(){
         Instance = this;
         DestPoints = new List<HexCell>();
-        loadingPath = new Dictionary<int, HexCell>();
-        closed = new Dictionary<int,HexCell>();
+        finalPath = new List<HexCell>();
+        closed = new HashSet<HexCell>();
         open = new Dictionary<HexCell, float>();
     }
 
@@ -29,12 +29,8 @@ public class PathManager : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
         if (hit.collider != null){
-            //collider处于Img上,而存储的是Img的父节点的Hash
-            //int tempHash = hit.collider.transform.parent.gameObject.GetHashCode();
-            //HexCell curCell = MapManager.Instance.CellObjects[tempHash];
 
             HexCell curCell = ObjectManager.GetClass<HexCell>(hit.collider.transform.parent.gameObject);
-
 
             if (Input.GetMouseButtonDown(0)) {
                 Begin = curCell;
@@ -45,6 +41,7 @@ public class PathManager : MonoBehaviour
             }
             if (Input.GetMouseButtonDown(2)) {
                 UnityEngine.SceneManagement.SceneManager.LoadScene("TestSc");
+                GameStaticData.random = new System.Random(1);
             }
         }
     }
@@ -55,6 +52,7 @@ public class PathManager : MonoBehaviour
     public void AddDestPoint(HexCell cell) {
         DestPoints.Add(cell);
         AStarFindPath(Begin, cell, FindNextCell);
+        ShowPath();
      }
     /// <summary>
     /// 取消选中的目的地
@@ -62,20 +60,33 @@ public class PathManager : MonoBehaviour
     public void CancelPathPoint(HexCell cell) {
         //AStarFindPath(cell, cell, HideAdj);
     }
+
+    public void ShowPath() {
+        HexCell cur = DestPoints[DestPoints.Count - 1];
+        while(cur != Begin) {
+            cur.Img.color = new Color(1,0,0);
+            cur = cur.prepathcell;
+        }
+    }
+
     /// <summary>
     /// 计算传入位置在当前选择路径中的G+H消耗,并返回
     /// </summary>
     public float SumCost(HexCell cur) {
-        float pathcost = cur.passCost;
-        //TODO:取消第一个from的值
-        //沿当前路径走过时的总消耗,充当G值
-        foreach(var cell in loadingPath) {
-            pathcost += cell.Value.passCost;
+        // G = 从起点走到当前节点的地形消耗
+        float pathcost = 0f;
+        if (cur.prepathcell == null) {
+            cur.dynamicost = 0f;
         }
-        //从该点到最新目的地的曼哈顿距离,充当H值
-        float eva = Vector2Int.Distance(cur.MapPos, DestPoints[DestPoints.Count-1].MapPos);
+        else {
+            cur.dynamicost = cur.prepathcell.dynamicost + cur.fieldcost;
+        }
+        pathcost = cur.dynamicost;
+
+        //从该点到最新目的地的曼哈顿距离,充当该点的H值
+        cur.destdis = Vector2Int.Distance(cur.MapPos, DestPoints[DestPoints.Count-1].MapPos);
         //G+H的值
-        pathcost += eva;
+        pathcost += cur.destdis;
 
         return pathcost;
       }
@@ -83,19 +94,18 @@ public class PathManager : MonoBehaviour
     /// <summary>
     /// 寻路
     /// </summary>
-    public void AStarFindPath(HexCell from,HexCell dest,Func<HexCell,HexCell>Judge) {
-        //将除了起点的节点加入loadingPath
-        if (from != Begin){
-            loadingPath.Add(from.GetHashCode(), from);
-        }
-        Debug.Log(from.GetHashCode() + "-=-" + from.MapPos);
-        HexCell nextCell = Judge(from);
+    public void AStarFindPath(HexCell from,HexCell dest,Func<HexCell,HexCell>FindFunc) {
 
-        if (!closed.ContainsKey(from.GetHashCode())) {
-            closed.Add(from.GetHashCode(), from);
+        float curCost = SumCost(from);
+
+        if (!open.ContainsKey(from)) {
+            open.Add(from, curCost);
         }
+
+        HexCell nextCell = FindFunc(from);
+
         //如果未找到则递归调用寻找
-        if(nextCell.GetHashCode() != dest.GetHashCode()) {
+        if(nextCell!= dest) {
             AStarFindPath(nextCell, dest, FindNextCell);
          }
         //找到时,返回上层递归
@@ -104,41 +114,46 @@ public class PathManager : MonoBehaviour
 
     public HexCell FindNextCell(HexCell cur) {
         //获取入参cell的邻接cells(不包括水行区域及loadingPath,两者已被放入closed并在传入之前剔除)
+        open.Remove(cur);
+        //只要作为过一次路径节点,则会被归入closed,防止纠错路径时再次回到该点
+        closed.Add(cur);
         List<HexCell> adj = AdjacentHex(cur);
 
-        float minCost = GameStaticData.infinite;
-        int minIndex = 0;
         //先算出当前邻接列表中的消耗最少的前进节点,并将所有节点加入open的待机节点
         for(int i=0;i<adj.Count ;i++) {
-            adj[i].Img.color = new Color(0, 1, 0);
-            float cost = SumCost(adj[i]);
-            adj[i].SetText(cost.ToString());
+
             //检查在open列表中是否存在该节点,不存在则添加.
             if (!open.ContainsKey(adj[i]))
             {
-                open.Add(adj[i], cost);
+                adj[i].prepathcell = cur;//直接为新临界点添加前继节点
+                open.Add(adj[i], SumCost(adj[i]));
             }
-            //如果是更少的消耗节点,则将最少消耗更新为该节点
-            if (minCost > cost) {
-                minCost = cost;
-                minIndex = i;
+            else {//检查以新节点作为临时前继节点,是否消耗更低
+                float newcost = adj[i].fieldcost + cur.dynamicost + Vector2Int.Distance(adj[i].MapPos, DestPoints[DestPoints.Count - 1].MapPos);
+                if (adj[i].dynamicost > newcost) //沿其他路径到该点的消耗比从此路径要大,则更新open中的路径(该节点的前继节点)
+                {
+                    //更新前继节点,保证路径的更新
+                    adj[i].prepathcell = cur;
+                    open[adj[i]] = SumCost(adj[i]);
+                }
             }
-        }
-        //TODO:如果open列表中有很靠前的前置节点比这个更优要怎么办.
-        //如果open中的该节点的消耗比当前的值要小,说明前面有节点通过更少的消耗抵达当前点,要更新路径
-        for (int i = 0;i<open.Count ;i++ ) {
-            
-        }
-        if (open[adj[minIndex]]<minCost) {
-            //去除路径中的当前节点,并更新open列表中的值
-            loadingPath.Remove(cur.GetHashCode());
-            open[cur] = SumCost(cur);
-            //adj[i].SetText(cost.ToString());
-            //将更小的前继节点作为路径值,并将其放入closed列表中  的  工作是在下一个递归做的
+            adj[i].Img.color = new Color(0, 1, 0);
+            adj[i].SetText(SumCost(adj[i]).ToString());
         }
 
-        adj[minIndex].Img.color = new Color(0, 0, 1);
-        return adj[minIndex];
+        float minCost = GameStaticData.infinite;
+        HexCell next = null;
+        //选择路径:选择open中的最小消耗节点,并沿其寻找新的路径
+        foreach(KeyValuePair<HexCell,float> pair in open) {
+            //选择消耗最小的作为next
+            if (pair.Value < minCost) {
+                next = pair.Key;
+                minCost = next.dynamicost + next.destdis;//G+h
+            }
+        }
+
+        next.Img.color = new Color(0, 0, 1);
+        return next;
     }
 
     public HexCell HideAdj(List<HexCell> adj)
@@ -169,17 +184,16 @@ public class PathManager : MonoBehaviour
         {
             //如果本身就属于closed,则在邻接表中删除该节点
             //(已经在当前路径的节点,也在closed中,所以不在下方再加入是否属于loadingPath的判断)
-            if (closed.ContainsKey(adj[i].GetHashCode()))
+            if (closed.Contains(adj[i]))
             {
                 adj.Remove(adj[i]);
             }
             else//如果本身不在,closed,再接着判别
             {
-
                 //如果属于不能在当前载具情况下行动的边境海,就将其剔除,并加入closed列表
                 if (adj[i].type == GameStaticData.FieldType.EdgeSea || adj[i].type == GameStaticData.FieldType.Lake)
                 {
-                    closed.Add(adj[i].GetHashCode(), adj[i]);
+                    closed.Add(adj[i]);
                     adj.Remove(adj[i]);
                 }
             }
