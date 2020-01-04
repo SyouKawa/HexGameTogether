8900ee6b16582cecd53f70492725a46a69240406
bisyoufish <chernoar_syoukawa_bisyoufish@outlook.com>
2020-01-04 13:43:06 +0800

    添加单例泛型基类

---
+++ Assets/Scripts/Tools/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 通用泛型单例类 
+/// 使用形式 class Type : Singleton<Type>{}
+/// </summary>
+public class Singleton<T> where T : new(){
+
+    private static T _instance;
+    public static T GetInstance(){
+
+        if (_instance == null){
+            _instance = new T();
+        }
+        return _instance;
+    }
+}
